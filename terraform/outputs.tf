9659fb354f798ee10e02c47e6f5e8d49bb7e3c2a
Walter Gaitan <walterg.steven@gmail.com>
2022-10-02 10:21:01 -0600

    FIX: outputs ADD: hello-api-gateway ADD images

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
-output "hello_base_url" {
-  value = aws_apigatewayv2_stage.dev.invoke_url
+output "hello_url" {
+  value = "${aws_apigatewayv2_stage.dev.invoke_url}/${aws_lambda_function.hello.function_name}"
 }
 
 output "goodbye_url" {
-  value = "https://${aws_apigatewayv2_stage.dev.invoke_url}/${aws_lambda_function.goodbye.function_name}"
+  value = "${aws_apigatewayv2_stage.dev.invoke_url}/${aws_lambda_function.goodbye.function_name}"
 }
 
 output "test_goodbye_bucket" {
