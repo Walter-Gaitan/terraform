8924046abcadfe53e5d2b464e58a59a7298afa1a
Walter Gaitan <walterg.steven@gmail.com>
2022-10-02 09:35:28 -0600

    Change name of s3 folder to goodbye change function.js to goodbye

---
+++ goodbye-lambda/function.js
@@ -0,0 +1,18 @@
+exports.handler = async (event) => {
+  console.log('Event: ', event);
+  let responseMessage = 'You did it! this is the second lambda function!';
+
+    if (event.queryStringParameters && event.queryStringParameters['Name'] && event.queryStringParameters['Name'] === 'goodbye') {
+        responseMessage = 'Goodbye, ' + event.queryStringParameters['Name'] + '!';
+    }
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: responseMessage
+        }),
+    };
+}
