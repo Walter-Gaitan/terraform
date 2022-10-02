1470451f2918967fdc6d859d5b55cd21987b27b6
Walter Gaitan <walterg.steven@gmail.com>
2022-10-02 10:23:24 -0600

    FIX: function goodbye

--- goodbye/function.js
+++ goodbye/function.js
@@ -1,8 +1,8 @@
 exports.handler = async (event) => {
-  console.log('Event: ', event);
-  let responseMessage = 'You did it! this is the second lambda function!';
+    console.log('Event: ', event);
+    let responseMessage = 'You did it! this is the second lambda function!';
 
-    if (event.queryStringParameters && event.queryStringParameters['Name'] && event.queryStringParameters['Name'] === 'goodbye') {
+    if (event.queryStringParameters && event.queryStringParameters['Name']) {
         responseMessage = 'Goodbye, ' + event.queryStringParameters['Name'] + '!';
     }
 
