d7a7cb2de0e6d41e1762374fd019fd8ce795ac1f
Restyled.io <commits@restyled.io>
2022-10-03 03:31:41 +0000

    Restyled by prettier

--- terraform/goodbye/function.js
+++ terraform/goodbye/function.js
@@ -1,16 +1,16 @@
 exports.handler = async (event) => {
-  console.log('Event: ', event);
-  let responseMessage = 'You did it! this is the second lambda function!';
+  console.log("Event: ", event);
+  let responseMessage = "You did it! this is the second lambda function!";
 
-  if (event.queryStringParameters && event.queryStringParameters['Name']) {
-    responseMessage = 'Goodbye, ' + event.queryStringParameters['Name'] + '!';
+  if (event.queryStringParameters && event.queryStringParameters["Name"]) {
+    responseMessage = "Goodbye, " + event.queryStringParameters["Name"] + "!";
   }
 
   return {
-    statusCode : 200,
-    headers : {
-      'Content-Type' : 'application/json',
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
     },
-    body : JSON.stringify({message : responseMessage}),
+    body: JSON.stringify({ message: responseMessage }),
   };
-}
+};
