d31d5e4bc82328443f865f28fd7b9141db23cc07
Restyled.io <commits@restyled.io>
2022-10-03 03:31:39 +0000

    Restyled by clang-format

--- terraform/hello/function.js
+++ terraform/hello/function.js
@@ -1,18 +1,16 @@
 exports.handler = async (event) => {
-    console.log('Event: ', event);
-    let responseMessage = 'You did it! this is the first lambda function!';
+  console.log('Event: ', event);
+  let responseMessage = 'You did it! this is the first lambda function!';
 
-    if (event.queryStringParameters && event.queryStringParameters['Name']) {
-        responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
-    }
+  if (event.queryStringParameters && event.queryStringParameters['Name']) {
+    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
+  }
 
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            message: responseMessage
-        }),
-    };
+  return {
+    statusCode : 200,
+    headers : {
+      'Content-Type' : 'application/json',
+    },
+    body : JSON.stringify({message : responseMessage}),
+  };
 };
