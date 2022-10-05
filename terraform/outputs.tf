7cdb1e1378218703248143a0a9cfd83324aab4a1
Walter Gaitan <walterg.steven@gmail.com>
2022-10-05 14:32:12 -0600

    update lambda name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,7 +5,3 @@ output "hello_url" {
 output "goodbye_url" {
   value = "${aws_apigatewayv2_stage.dev.invoke_url}/${aws_lambda_function.goodbye.function_name}"
 }
-
-output "test_goodbye_bucket" {
-  value = random_pet.test_bucket_name.id
-}
